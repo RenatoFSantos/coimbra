7d5ea60dfed3f97a5fe503ba5f1aeb092e329d25
RenatoFSantos <renato.insigne@gmail.com>
2026-03-16 20:07:40 -0300

    feat: config standalone project

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig = {
+    output: 'standalone',
+}
 
 export default nextConfig;
+
